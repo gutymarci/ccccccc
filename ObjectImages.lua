e2b9a5f55fe9ec993a0224fd2cea9e52d6dcb5d4
gutymarci <gutymarci@gmail.com>
2026-02-26 21:20:02 -0500

    Rewrite marble dropper server script with complete reward flow

---
+++ ObjectImages.lua
@@ -0,0 +1,17 @@
+local ObjectImages = {
+	["Dropper1"] = "rbxassetid://90573182654918",
+	["Carpet"] = "rbxassetid://90573182654918",
+	["Carpet Corner"] = "rbxassetid://79903675125375",
+	["Door"] = "rbxassetid://79903675125375",
+	["Fence"] = "rbxassetid://102013563230339",
+	["Fence Corner"] = "rbxassetid://128979734676584",
+	["Floor"] = "rbxassetid://72186129717638",
+	["OwnerDoor"] = "rbxassetid://74501378012088",
+	["Plant"] = "rbxassetid://110136427744019",
+	["Template"] = "rbxassetid://90573182654918",
+	["Wall"] = "rbxassetid://129706629571716",
+	["Window"] = "rbxassetid://74056161831009",
+	["Collecter"] = "rbxassetid://74056161831009",
+}
+
+return ObjectImages
